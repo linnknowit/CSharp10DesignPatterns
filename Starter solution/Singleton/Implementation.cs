b8b22e56a8103e7b7e74b52ff2cd43ef485d89b8
Linn Haraldsson <linn.haraldsson@knowit.se>
2023-03-16 14:29:41 +0100

    Added singleton pattern implementation

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,29 @@
 ﻿namespace Singleton
 {
-  
+  public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger 
+            = new Lazy<Logger>(() => new Logger());
+
+        //private static Logger? _instance;
+
+        public static Logger? Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //_instance ??= new Logger();
+                //return _instance;
+            }
+        }
+        protected Logger() 
+        {
+            
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
 }
